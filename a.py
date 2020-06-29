c49535f8aa7857bfd55538bfa283fe22f7998c54
eshaang <67595076+eshaang@users.noreply.github.com>
2020-06-29 11:09:35 -0400

    Add files via upload

---
+++ a.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a number."))
+#b = int(input("Enter a number.""))
+#c = int(input("Enter a number.""))
+#if b > a and b>c:
+#	print("number two is greater than number one and three")
+#elif a>b and a>c:
+#	print("number one is greater than number two and three ")
+#else:
+#	print("number three is greater than one or two")
+#if a % 2 = 1:
+#	print("odd")
+#else:
+#	print("even")
+if a % 5 == 0:
+	print("divisible by five")
+else:
+	print("not divisible by five")
