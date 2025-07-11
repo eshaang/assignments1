9b5fcaaeaaa492353d56f54f4a98e39c379b2d31
eshaang <67595076+eshaang@users.noreply.github.com>
2025-07-11 14:10:19 -0400

    Update a.py

--- a.py
+++ a.py
@@ -1,17 +1,1 @@
-a = int(input("Enter a number."))
-#b = int(input("Enter a number.""))
-#c = int(input("Enter a number.""))
-#if b > a and b>c:
-#	print("number two is greater than number one and three")
-#elif a>b and a>c:
-#	print("number one is greater than number two and three ")
-#else:
-#	print("number three is greater than one or two")
-#if a % 2 = 1:
-#	print("odd")
-#else:
-#	print("even")
-if a % 5 == 0:
-	print("divisible by five")
-else:
-	print("not divisible by five")
+
